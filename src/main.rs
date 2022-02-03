d1041cf7f1a29a1d5fa907fb5e8c5634ebd07887
Maurycy Z <10maurycy10@gmail.com>
2022-02-03 08:51:46 -0800

    use anyhow

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,43 @@ use std::io::BufRead;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::ffi::{OsString, OsStr};
+use anyhow::Context;
 
-fn main() {
+fn checkfile(file: &str, magic :&Cookie, db: &HashMap<String, HashSet<String>>) -> anyhow::Result<()> {
+    let mimetype = magic.file(file.clone()).context("failed to find mime type of file.")?;
+    let ext = std::path::Path::new(file).extension();
+            
+    let ext = match ext {
+        Some(ext) => ext.to_str().context("failed to get file extention")?,
+        None => ""
+    };
+    if (&mimetype != &"inode/directory" && &mimetype != &"inode/simlink") {
+        match db.get(&mimetype) {
+            None => println!("{}: is {}",&file, &mimetype),
+            Some(exts) => if !exts.contains(&*ext) {
+                println!("{}: is {}",&file, &mimetype);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn run() -> anyhow::Result<()> {
     eprint!("Loading file extention data... ");
-    let db = std::fs::read_to_string("db.json").unwrap();
-    let db :HashMap<String, HashSet<String>> = serde_json::from_str(&db).unwrap();
+    let db = std::fs::read_to_string("db.json").context("failed getting db.json")?;
+    let db :HashMap<String, HashSet<String>> = serde_json::from_str(&db).context("failed parsing file ext db")?;
     eprint!("[OK]\n");
     eprint!("Loading magic number data ... ");
-    let cookie = Cookie::open(CookieFlags::from_bits(0b000000010000).unwrap()).unwrap();
+    let cookie = Cookie::open(CookieFlags::from_bits(0b000000010000).context("failed initalizing libmagic flags")?).context("failed initalizing libmagic")?;
     cookie.load(&["magic.mgc"]);
     eprint!("[OK]\n");
     for line in std::io::stdin().lock().lines() {
             let line = line.unwrap();
-            
-            let mimetype = cookie.file(line.clone()).unwrap();
-            let ext = std::path::Path::new(&line).extension();
-            
-            let ext =match ext {
-                Some(ext) => ext.to_str().unwrap(),
-                None => ""
-            };
-            if (&mimetype != &"inode/directory" && &mimetype != &"inode/simlink") {
-                match db.get(&mimetype) {
-                    None => println!("{}: is {}",&line, &mimetype),
-                    Some(exts) => if !exts.contains(&*ext) {
-                        println!("{}: is {}",&line, &mimetype);
-                    }
-                }
-            }            
-    }
+            checkfile(&line,&cookie,&db).with_context(|| format!("failed to prossess file {}",line))?;
+    };
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    run()
 }
